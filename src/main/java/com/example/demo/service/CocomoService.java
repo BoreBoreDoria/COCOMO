52e0c6798f6147c6efe5a53a9fc2c6cda5e9cb4a
Nikita Rochev <survivalnightgame@gmail.com>
2022-04-12 09:00:29 +0300

    fix

--- src/main/java/com/example/demo/service/CocomoService.java
+++ src/main/java/com/example/demo/service/CocomoService.java
@@ -11,15 +11,15 @@ public class CocomoService {
         Double pm = projectTypeLight.getA()*Math.pow(size, projectTypeLight.getB());
         Double tm = projectTypeLight.getC()+Math.pow(pm, projectTypeLight.getD());
         Double h = pm/tm;
-        return String.format("Расчёт закончен: PM = %s (трудоёмкость чел. х мес.) , " +
-                "TM = %s (время разработки в календарных месяцах) Разработчики: %s человек", pm.longValue(), tm.longValue(), h.longValue());
+        return String.format("Расчёт закончен: PM = %.2f (трудоёмкость чел. х мес.) , " +
+                "TM = %.2f (время разработки в календарных месяцах) Разработчики: %.2f человек", pm, tm, h);
     }
 
     public String calculateCoComoHard(Long size, Double eaf, ProjectTypeHard projectTypeHard){
         Double pm = eaf * projectTypeHard.getA() * Math.pow(size, projectTypeHard.getB());
         Double tm = projectTypeHard.getC()+Math.pow(pm, projectTypeHard.getD());
         Double h = pm/tm;
-        return String.format("Расчёт закончен: PM = %s (трудоёмкость чел. х мес.) , " +
-                "TM = %s (время разработки в календарных месяцах) Разработчики: %s человек", pm.longValue(), tm.longValue(), h.longValue());
+        return String.format("Расчёт закончен: PM = %.2f (трудоёмкость чел. х мес.) , " +
+                "TM = %.2f (время разработки в календарных месяцах) Разработчики: %.2f человек", pm, tm, h);
     }
 }
