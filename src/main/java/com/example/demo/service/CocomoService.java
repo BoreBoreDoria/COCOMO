293049b1cba1c1c20d253ecb4938fc83ed228fc1
Nikita Rochev <survivalnightgame@gmail.com>
2022-04-12 06:34:16 +0300

    fix

--- src/main/java/com/example/demo/service/CocomoService.java
+++ src/main/java/com/example/demo/service/CocomoService.java
@@ -10,13 +10,17 @@ public class CocomoService {
     public String calculateCocomoBasic(Long size, ProjectTypeLight projectTypeLight) {
         Double pm = projectTypeLight.getA()*Math.pow(size, projectTypeLight.getB());
         Double tm = projectTypeLight.getC()+Math.pow(pm, projectTypeLight.getD());
-        return String.format("Расчёт закончен: PM = %s, TM = %s", pm.longValue(), tm.longValue());
+        Double h = pm/tm;
+        return String.format("Расчёт закончен: PM = %s (трудоёмкость чел. х мес.) , " +
+                "TM = %s (время разработки в календарных месяцах) Разработчики: %s человек", pm.longValue(), tm.longValue(), h.longValue());
     }
 
     public String calculateCoComoHard(Long size, Double eaf, ProjectTypeHard projectTypeHard){
         Double pm = eaf * projectTypeHard.getA() * Math.pow(size, projectTypeHard.getB());
         Double tm = projectTypeHard.getC()+Math.pow(pm, projectTypeHard.getD());
-        return String.format("Расчёт закончен: PM = %s, TM = %s", pm.longValue(), tm.longValue());
+        Double h = pm/tm;
+        return String.format("Расчёт закончен: PM = %s (трудоёмкость чел. х мес.) , " +
+                "TM = %s (время разработки в календарных месяцах) Разработчики: %s человек", pm.longValue(), tm.longValue(), h.longValue());
     }
 
 
